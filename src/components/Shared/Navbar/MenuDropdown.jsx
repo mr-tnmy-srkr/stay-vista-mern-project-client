c73058922cae92d4c1503596b21551c613980245
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-25 11:47:12 +0530

    nodemailer transporter added

--- src/components/Shared/Navbar/MenuDropdown.jsx
+++ src/components/Shared/Navbar/MenuDropdown.jsx
@@ -1,63 +1,92 @@
-import { AiOutlineMenu } from 'react-icons/ai'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import useAuth from '../../../hooks/useAuth'
-import avatarImg from '../../../assets/images/placeholder.jpg'
+import { AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import avatarImg from "../../../assets/images/placeholder.jpg";
+import { becomeHost } from "../../../api/auth";
+import toast from "react-hot-toast";
+import useRole from "../../../hooks/useRole";
+import HostModal from "../../Modal/HostRequestModal";
 
 const MenuDropdown = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const { user,logOut } = useAuth()
+  const [role] = useRole()
+  const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const { user, logOut } = useAuth();
+
+  const modalHandler = async () => {
+    try {
+      const data = await becomeHost(user?.email);
+      console.log(data);
+      if (data.modifiedCount > 0) {
+        toast.success("Success,Please wait for admin Confirmation");
+      } else {
+        toast.success("Please, wait for admin Approval 🚫");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsModalOpen(false);
+    }
+  };
 
   return (
-    <div className='relative'>
-      <div className='flex flex-row items-center gap-3'>
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
         {/* Become A Host btn */}
-        <div className='hidden md:block'>
-          <button className='disabled:cursor-not-allowed cursor-pointer hover:bg-neutral-100 py-3 px-4 text-sm font-semibold rounded-full  transition'>
-           {user?.displayName || " Your home"}
-          </button>
+        <div className="hidden md:block">
+          {(!user || !role || role === "guest") && 
+          <button
+            onClick={() => setIsModalOpen(true)}
+            disabled = {!user}
+            className="disabled:cursor-not-allowed cursor-pointer hover:bg-neutral-100 py-3 px-4 text-sm font-semibold rounded-full  transition"
+          >
+            Host Your Name
+          </button>}
         </div>
         {/* Dropdown btn */}
         <div
           onClick={() => setIsOpen(!isOpen)}
-          className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
-          <div className='hidden md:block'>
+          <div className="hidden md:block">
             {/* Avatar */}
             <img
-              className='rounded-full'
-              referrerPolicy='no-referrer'
+              className="rounded-full"
+              referrerPolicy="no-referrer"
               src={user && user.photoURL ? user.photoURL : avatarImg}
-              alt='profile'
-              height='30'
-              width='30'
+              alt="profile"
+              height="30"
+              width="30"
             />
           </div>
         </div>
       </div>
       {isOpen && (
-        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm'>
-          <div className='flex flex-col cursor-pointer'>
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
             <Link
-              to='/'
-              className='block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+              to="/"
+              className="block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold"
             >
               Home
             </Link>
 
-           
             {user ? (
               <>
                 <Link
-                  to='/dashboard'
-                  className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                  to="/dashboard"
+                  className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
                 >
                   Dashboard
                 </Link>
                 <div
                   onClick={logOut}
-                  className='px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer'
+                  className="px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer"
                 >
                   Logout
                 </div>
@@ -65,14 +94,14 @@ const MenuDropdown = () => {
             ) : (
               <>
                 <Link
-                  to='/login'
-                  className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                  to="/login"
+                  className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
                 >
                   Login
                 </Link>
                 <Link
-                  to='/signup'
-                  className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                  to="/signup"
+                  className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
                 >
                   Sign Up
                 </Link>
@@ -81,8 +110,13 @@ const MenuDropdown = () => {
           </div>
         </div>
       )}
+      <HostModal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        modalHandler={modalHandler}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MenuDropdown
+export default MenuDropdown;
